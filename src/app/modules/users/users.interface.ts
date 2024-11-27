7ee5858e92a82b94bf3cae46275625e41337c9b4
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-11-27 23:27:01 +0600

    Add user model fields and update interface for enhanced user management

--- src/app/modules/users/users.interface.ts
+++ src/app/modules/users/users.interface.ts
@@ -1,3 +1,8 @@
 export interface UsersInterface {
   id: string;
+  password: string;
+  needsPasswordChange: boolean;
+  role: 'SUPER_ADMIN' | 'ADMIN' | 'FACULTY' | 'STUDENT';
+  status: 'ACTIVE' | 'BLOCKED';
+  isDeleted: boolean;
 }
--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,5 +1,45 @@
 import mongoose from 'mongoose';
+import { UsersInterface } from './users.interface';
 
-const UsersSchema = new mongoose.Schema({});
+const UsersSchema = new mongoose.Schema<UsersInterface>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['SUPER_ADMIN', 'ADMIN', 'FACULTY', 'STUDENT'],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['ACTIVE', 'BLOCKED'],
+    },
+    isDeleted: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
 
 export const Users = mongoose.model('Users', UsersSchema);
