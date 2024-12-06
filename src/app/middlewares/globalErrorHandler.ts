e599d191ac44e79cf93ce9f0796e3b4a20d9a080
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-06 13:02:15 +0600

    Refactor global error handler to improve error response structure; include error sources and final error details

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,20 +1,29 @@
-import { NextFunction, Request, Response } from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+
+import { ErrorRequestHandler } from 'express';
 import httpStatus from 'http-status';
 
-const globalErrorHandler = (
-  err: Error,
-  _req: Request,
-  res: Response,
-  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  next: NextFunction,
-) => {
+type ErrorSourcesType = {
+  message: string;
+  path: string;
+}[];
+
+const globalErrorHandler: ErrorRequestHandler = (err, _req, res, next) => {
   const statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-  const message = err.message || 'Something went wrong!';
+  const message = 'Something went wrong!';
+  const errorSources: ErrorSourcesType = [
+    {
+      message: 'Something went wrong!',
+      path: '',
+    },
+  ];
 
   res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    finalError: err,
   });
 };
 
