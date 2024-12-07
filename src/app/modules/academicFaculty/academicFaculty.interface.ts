f9831ad72fcadf68fdfb7bd18a3f0297cd01f72c
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-07 18:42:15 +0600

    Update AcademicFaculty interface to reflect revised department names and structure

--- src/app/modules/academicFaculty/academicFaculty.interface.ts
+++ src/app/modules/academicFaculty/academicFaculty.interface.ts
@@ -1,17 +1,11 @@
 /*
-Department of Business Administration
-Department of Civil Engineering
-Department of Computer Science and Engineering
-Department of Education
-Department of Electrical and Electronic Engineering
-Department of English
-Department of Fashion Design and Technology
-Department of Islamic Studies
-Department of Law
-Department of Mathematics
-Department of Physics
-Department of Physical Education
-Department of Textile Engineering
+  Engineering and Technology
+  Business Administration
+  Arts and Humanities
+  Science and Technology
+  Law
+  Social Sciences
+  Medicine
 */
 
 export interface AcademicFacultyInterface {
