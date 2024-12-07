3544531723ab0f4e6c7f2a39101db3cfc9a21a76
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-08 01:14:06 +0600

    Add server shutdown handling for unhandled promise rejections and exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
@@ -6,12 +7,14 @@ import config from './app/config';
 const database_url = config.database_url as string;
 const port = config.port;
 
+let server: Server;
+
 async function main() {
   try {
     await mongoose.connect(database_url);
     console.log('🛢️  Database connection successful ✅');
 
-    app.listen(port, () => {
+    server = app.listen(port, () => {
       console.log(`🎯 Server listening on port: ${port}`);
     });
   } catch (err) {
@@ -20,3 +23,18 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
