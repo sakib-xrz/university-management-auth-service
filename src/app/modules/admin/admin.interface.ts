4aa6f001aeff62adc364ecac11932b694be43f4d
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-08 18:02:25 +0600

    Add Admin model and interface with validation; implement virtuals and query middleware for soft delete

--- src/app/modules/admin/admin.interface.ts
+++ src/app/modules/admin/admin.interface.ts
@@ -1,1 +1,22 @@
-export interface AdminInterface {}
+import { Types } from 'mongoose';
+
+export interface AdminInterface {
+  id: string;
+  user: Types.ObjectId;
+  name: {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+  };
+  designation: string;
+  gender: 'MALE' | 'FEMALE' | 'OTHER';
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  profileImage?: string;
+  isDeleted: boolean;
+}
--- src/app/modules/admin/admin.model.ts
+++ src/app/modules/admin/admin.model.ts
@@ -1,5 +1,108 @@
+import { AdminInterface } from './admin.interface';
 import mongoose from 'mongoose';
 
-const AdminSchema = new mongoose.Schema({});
+const AdminSchema = new mongoose.Schema<AdminInterface>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    designation: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ['MALE', 'FEMALE', 'OTHER'],
+      required: true,
+    },
+    dateOfBirth: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    emergencyContactNo: {
+      type: String,
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    permanentAddress: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      default: '',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
 
-export const Admin = mongoose.model('Admin', AdminSchema);
+// virtual
+AdminSchema.virtual('fullName').get(function () {
+  const { firstName, middleName, lastName } = this.name;
+  return `${firstName} ${middleName ? middleName : ''} ${lastName}`;
+});
+
+// Query Middleware
+AdminSchema.pre('find', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+AdminSchema.pre('findOne', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+
+AdminSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
+
+export const Admin = mongoose.model<AdminInterface>('Admin', AdminSchema);
