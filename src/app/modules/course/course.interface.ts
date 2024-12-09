9113e4907c8fecf8606d427896851f71ae15c006
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-09 18:53:35 +0600

    Enhance CourseInterface with detailed properties and prerequisite course type

--- src/app/modules/course/course.interface.ts
+++ src/app/modules/course/course.interface.ts
@@ -1,1 +1,15 @@
-export interface CourseInterface {}
+import { Types } from 'mongoose';
+
+export type PrerequisiteCourseType = {
+  course: Types.ObjectId;
+  isDeleted: boolean;
+};
+
+export interface CourseInterface {
+  title: string;
+  prefix: string;
+  code: number;
+  credits: number;
+  isDeleted: boolean;
+  prerequisiteCourses: PrerequisiteCourseType[];
+}
