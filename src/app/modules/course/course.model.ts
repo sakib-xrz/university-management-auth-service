13c437d2486c550dec86ab193b51905fefafcd7b
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-09 18:55:38 +0600

    Refactor Course model to include detailed schema properties and prerequisite courses

--- src/app/modules/course/course.model.ts
+++ src/app/modules/course/course.model.ts
@@ -1,5 +1,50 @@
 import mongoose from 'mongoose';
+import { CourseInterface } from './course.interface';
 
-const CourseSchema = new mongoose.Schema({});
+const CourseSchema = new mongoose.Schema<CourseInterface>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    prefix: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: Number,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    prerequisiteCourses: [
+      {
+        course: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Course',
+          required: true,
+        },
+        isDeleted: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
 
-export const Course = mongoose.model('Course', CourseSchema);
+export const Course = mongoose.model<CourseInterface>('Course', CourseSchema);
