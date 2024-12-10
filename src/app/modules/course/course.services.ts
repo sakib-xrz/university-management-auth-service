c90a781bb7b2f75a1cc4eb09806cc0fef7c2cbc3
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-10 13:34:35 +0600

    Add UpdateCourse function to manage course updates and prerequisite courses

--- src/app/modules/course/course.services.ts
+++ src/app/modules/course/course.services.ts
@@ -72,6 +72,75 @@ const GetCourseById = async (id: string) => {
   return course;
 };
 
+const UpdateCourse = async (id: string, payload: Partial<CourseInterface>) => {
+  const course = await Course.findById(id);
+
+  if (!course) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Course not found');
+  }
+
+  const { prerequisiteCourses, ...restUpdatedFields } = payload;
+
+  await Course.findByIdAndUpdate(
+    id,
+    {
+      ...restUpdatedFields,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  if (prerequisiteCourses && prerequisiteCourses.length) {
+    const deletablePrerequisiteCourses = prerequisiteCourses
+      .filter((el) => el.course && el.isDeleted)
+      .map((el) => el.course);
+
+    const creatablePrerequisiteCourses = prerequisiteCourses
+      .filter((el) => el.course && !el.isDeleted)
+      .map((el) => el.course);
+
+    if (deletablePrerequisiteCourses.length) {
+      await Course.findByIdAndUpdate(id, {
+        $pull: {
+          prerequisiteCourses: {
+            course: { $in: deletablePrerequisiteCourses },
+          },
+        },
+      });
+    }
+
+    if (creatablePrerequisiteCourses.length) {
+      for (let i = 0; i < creatablePrerequisiteCourses.length; i++) {
+        const course = await Course.findById(creatablePrerequisiteCourses[i]);
+        if (!course) {
+          throw new AppError(
+            httpStatus.BAD_REQUEST,
+            `Prerequisite course with id "${creatablePrerequisiteCourses[i]}" not found`,
+          );
+        }
+      }
+
+      await Course.findByIdAndUpdate(id, {
+        $addToSet: {
+          prerequisiteCourses: {
+            $each: creatablePrerequisiteCourses.map((el) => ({
+              course: el,
+            })),
+          },
+        },
+      });
+    }
+
+    const updatedCourse = await Course.findById(id).populate(
+      'prerequisiteCourses.course',
+    );
+
+    return updatedCourse;
+  }
+};
+
 const DeleteCourse = async (id: string) => {
   const course = await Course.findByIdAndUpdate(
     id,
@@ -87,6 +156,12 @@ const DeleteCourse = async (id: string) => {
   }
 };
 
-const CourseService = { CreateCourse, GetCourses, GetCourseById, DeleteCourse };
+const CourseService = {
+  CreateCourse,
+  GetCourses,
+  GetCourseById,
+  UpdateCourse,
+  DeleteCourse,
+};
 
 export default CourseService;
