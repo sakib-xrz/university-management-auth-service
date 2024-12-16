fb8ee0e6809015dfccad734519edb1bb8498b125
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-16 11:50:14 +0600

    Add validation schema for semester registration using Zod

--- src/app/modules/semesterRegistration/semesterRegistration.validation.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.validation.ts
@@ -1,3 +1,50 @@
-const SemesterRegistrationValidation = {};
+import { z } from 'zod';
+import SemesterRegistrationConstants from './semesterRegistration.constant';
+
+const CreateSchema = z.object({
+  body: z.object({
+    academicSemester: z.string({
+      required_error: 'Academic semester id is required',
+      invalid_type_error: 'Academic semester id must be a string',
+    }),
+    status: z
+      .enum(
+        [...(SemesterRegistrationConstants.Status as [string, ...string[]])],
+        {
+          message:
+            'Status must be one of the following values: UPCOMING, ONGOING, ENDED',
+        },
+      )
+      .optional(),
+    startDateTime: z
+      .string({
+        required_error: 'Start date time is required',
+        invalid_type_error: 'Start date time must be a string',
+      })
+      .datetime({
+        message: 'Start date time must be a valid date time',
+      }),
+    endDateTime: z
+      .string({
+        required_error: 'End date time is required',
+        invalid_type_error: 'End date time must be a string',
+      })
+      .datetime({
+        message: 'End date time must be a valid date time',
+      }),
+    minCredit: z
+      .number({
+        invalid_type_error: 'Min credit must be a number',
+      })
+      .optional(),
+    maxCredit: z
+      .number({
+        invalid_type_error: 'Max credit must be a number',
+      })
+      .optional(),
+  }),
+});
+
+const SemesterRegistrationValidation = { CreateSchema };
 
 export default SemesterRegistrationValidation;
