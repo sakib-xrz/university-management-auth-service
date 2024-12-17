0f6740120a9c388b8afa72c77595df1009d10666
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-17 20:26:50 +0600

    Define OfferedCourse schema with detailed properties and validation requirements

--- src/app/modules/offeredCourse/offeredCourse.model.ts
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -1,5 +1,71 @@
 import mongoose from 'mongoose';
+import { OfferedCourseInterface } from './offeredCourse.interface';
+import OfferedCourseConstants from './offeredCourse.constant';
 
-const OfferedCourseSchema = new mongoose.Schema({});
+const OfferedCourseSchema = new mongoose.Schema<OfferedCourseInterface>(
+  {
+    semesterRegistration: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'SemesterRegistration',
+      required: true,
+    },
+    academicSemester: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      required: true,
+    },
+    academicFaculty: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+      required: true,
+    },
+    academicDepartment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'AcademicDepartment',
+      required: true,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+    faculty: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Faculty',
+      required: true,
+    },
+    maxCapacity: {
+      type: Number,
+      required: true,
+    },
+    section: {
+      type: Number,
+      required: true,
+    },
+    days: {
+      type: [String],
+      enum: OfferedCourseConstants.Days,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
 
-export const OfferedCourse = mongoose.model('OfferedCourse', OfferedCourseSchema);
+export const OfferedCourse = mongoose.model<OfferedCourseInterface>(
+  'OfferedCourse',
+  OfferedCourseSchema,
+);
